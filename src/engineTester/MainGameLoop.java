1a786e35d8d502e8b7888cdc2a9cc4b04fbb52b7
Soumik Rakshit <19soumik.rakshit96@gmail.com>
2019-03-10 10:04:45 +0530

    Add Cube Rotation

--- src/engineTester/MainGameLoop.java
+++ src/engineTester/MainGameLoop.java
@@ -24,20 +24,78 @@ public class MainGameLoop {
 		Renderer renderer = new Renderer(shader);
 		
 		float[] vertices = {
-				-0.5f, 0.5f, 0f,
-				-0.5f, -0.5f, 0f,
-				0.5f, -0.5f, 0f,
-				0.5f, 0.5f, 0f,
-		};
+				-0.5f, 0.5f, 0,
+				-0.5f, -0.5f, 0,
+				0.5f, -0.5f, 0,
+				0.5f, 0.5f, 0,
+
+				-0.5f, 0.5f, 1,
+				-0.5f, -0.5f, 1,
+				0.5f, -0.5f, 1,
+				0.5f, 0.5f, 1,
+
+				0.5f, 0.5f, 0,
+				0.5f, -0.5f, 0,
+				0.5f, -0.5f, 1,
+				0.5f, 0.5f, 1,
+
+				-0.5f, 0.5f, 0,
+				-0.5f, -0.5f, 0,
+				-0.5f, -0.5f, 1,
+				-0.5f, 0.5f, 1,
+
+				-0.5f, 0.5f, 1,
+				-0.5f, 0.5f, 0,
+				0.5f, 0.5f, 0,
+				0.5f, 0.5f, 1,
+
+				-0.5f, -0.5f, 1,
+				-0.5f, -0.5f, 0,
+				0.5f, -0.5f, 0,
+				0.5f, -0.5f, 1 
+        };
 		
 		float[] textureCoordinates = {
 				0, 0,
 				0, 1,
 				1, 1,
-				1, 0
-		};
+				1, 0,
+				0, 0,
+				0, 1,
+				1, 1,
+				1, 0,
+				0, 0,
+				0, 1,
+				1, 1,
+				1, 0,
+				0, 0,
+				0, 1,
+				1, 1,
+				1, 0,
+				0, 0,
+				0, 1,
+				1, 1,
+				1, 0,
+				0, 0,
+				0, 1,
+				1, 1,
+				1, 0       
+        };
 		
-		int[] indices = {0, 1, 3, 3, 1, 2};
+		int[] indices = {
+				0, 1, 3,
+				3, 1, 2,
+				4, 5, 7,
+				7, 5, 6,
+				8, 9, 11,
+				11, 9, 10,
+				12, 13, 15,
+				15, 13, 14,
+				16, 17, 19,
+				19, 17, 18,
+				20, 21, 23,
+				23, 21, 22
+        };
 		
 		
 		ArrayList<RawModel> modelList = new ArrayList<>();
@@ -48,7 +106,7 @@ public class MainGameLoop {
 		modelList.add(new RawModel(vertices, textureCoordinates, indices));
 		textureList.add( new ModelTexture(modelList.get(0).loadTexture("marble_texture")));
 		texturedModelList.add(new TexturedModel(modelList.get(0), textureList.get(0)));
-		entityList.add(new Entity(texturedModelList.get(0), new Vector3f(0, 0, -1), 0, 0, 0, 1));
+		entityList.add(new Entity(texturedModelList.get(0), new Vector3f(0, 0, -5), 0, 0, 0, 1));
 		
 		Camera camera = new Camera();
 		
@@ -59,8 +117,7 @@ public class MainGameLoop {
 		while(!DisplayManager.isCloseRequested()) {
 			
 			// Game Logic
-			entityList.get(0).increasePosition(0, 0, -0.01f);
-			entityList.get(0).increaseRotation(0, 1, 0);
+			entityList.get(0).increaseRotation(1, 1, 0);
 			camera.move();
 			
 			// Render Frame
